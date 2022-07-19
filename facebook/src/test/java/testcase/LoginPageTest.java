194e2994fcabc7daa611aa5a1cd5127c7cc47e2c
Madhumati1809 <madhuswami1809@gmail.com>
2022-07-19 21:36:41 +0530

    test class modified

--- facebook/src/test/java/testcase/LoginPageTest.java
+++ facebook/src/test/java/testcase/LoginPageTest.java
@@ -10,11 +10,15 @@ import base.BaseTest;
 public class LoginPageTest extends BaseTest {
 	
 	@Test
-	public void loginTest() {
+	public void loginTest() throws InterruptedException {
 		
 		driver.findElement(By.xpath(loc.getProperty("LoginElement")));
 		driver.findElement(By.xpath(loc.getProperty("LoginElement"))).click();
+		driver.findElement(By.xpath(loc.getProperty("username"))).sendKeys("");
+		driver.findElement(By.xpath(loc.getProperty("password"))).sendKeys("Tatu@12345");
+		driver.findElement(By.xpath(loc.getProperty("LoginButton"))).click();
 
+		
 	}
 
 }
