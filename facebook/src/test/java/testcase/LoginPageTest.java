c768f889756929bf9a65f41eaef181ab858d8d3b
Madhumati1809 <madhuswami1809@gmail.com>
2022-07-20 23:15:02 +0530

    screenshot feature added

--- facebook/src/test/java/testcase/LoginPageTest.java
+++ facebook/src/test/java/testcase/LoginPageTest.java
@@ -1,6 +1,14 @@
 package testcase;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -15,10 +23,14 @@ public class LoginPageTest extends BaseTest {
 
 
 	@Test
-	public void loginTest() throws InterruptedException {
+	public void loginTest() throws InterruptedException, IOException {
 		
-		String username="";
-		String password="";
+		String username="swamimadhumati@gmail.com";
+		
+		String password="Tatu@12345";
+		
+		Date currentdate= new Date();
+		String screenshotname =currentdate.toString().replace(" ", "-").replace(":", "-");
 
 		driver.findElement(By.xpath(loc.getProperty("LoginElement")));
 		driver.findElement(By.xpath(loc.getProperty("LoginElement"))).click();
@@ -29,17 +41,29 @@ public class LoginPageTest extends BaseTest {
 			driver.findElement(By.xpath(loc.getProperty("LoginButton"))).click();
 			Thread.sleep(5000);
 			String error_color=driver.findElement(By.xpath(loc.getProperty("username"))).getCssValue("border-top-color");
+			
+			File screenshotfile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshotfile, new File(".//screenshots//"+screenshotname+".png"));
+			
 			if(error_color.equals(loc.getProperty("error_color"))) {
 				String error_msg=driver.findElement(By.xpath(loc.getProperty("username_err_ele"))).getText();
 				softAssert.assertEquals(error_msg,loc.getProperty("user_err_msg"));
 			}
 		}else if(password.isEmpty()){
 			driver.findElement(By.xpath(loc.getProperty("LoginButton"))).click();
+			File screenshotfile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshotfile, new File(".//screenshots//"+screenshotname+".png"));
 			Thread.sleep(5000);
 			String error_msg=driver.findElement(By.xpath(loc.getProperty("pass_error_msg_ele"))).getText();
 			softAssert.assertEquals(error_msg,loc.getProperty("pass_err_msg"));
 		}else {
 			driver.findElement(By.xpath(loc.getProperty("LoginButton"))).click();
+			Thread.sleep(10000);
+			File screenshotfile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshotfile, new File(".//screenshots//"+screenshotname+".png"));
+			
+
+			
 		}
 
 
