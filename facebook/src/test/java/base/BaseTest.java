44e0fd198283546bad39b8cbd0378d29bf2cab5b
Madhumati1809 <madhuswami1809@gmail.com>
2022-07-20 13:47:43 +0530

    commit changes

--- facebook/src/test/java/base/BaseTest.java
+++ facebook/src/test/java/base/BaseTest.java
@@ -40,6 +40,7 @@ public class BaseTest {
 
 	@AfterMethod
 	public void teardown() {
+		
 		driver.close();
 	}
 
--- facebook/src/test/java/testcase/LoginPageTest.java
+++ facebook/src/test/java/testcase/LoginPageTest.java
@@ -9,18 +9,32 @@ import base.BaseTest;
 
 
 public class LoginPageTest extends BaseTest {
-	
+
 	SoftAssert softAssert = new SoftAssert();
+
+
+
 	@Test
 	public void loginTest() throws InterruptedException {
-		
+
 		driver.findElement(By.xpath(loc.getProperty("LoginElement")));
 		driver.findElement(By.xpath(loc.getProperty("LoginElement"))).click();
-		driver.findElement(By.xpath(loc.getProperty("username"))).sendKeys("");
+		driver.findElement(By.xpath(loc.getProperty("username"))).sendKeys("swamimadhumati@gmail.com");
 		driver.findElement(By.xpath(loc.getProperty("password"))).sendKeys("Tatu@12345");
+		String windowHandle=driver.getWindowHandle();
+		System.out.println(windowHandle);
 		driver.findElement(By.xpath(loc.getProperty("LoginButton"))).click();
 
-		
+		Thread.sleep(10000);
+
+		String username_error_color=driver.findElement(By.xpath(loc.getProperty("username"))).getCssValue("border-top-color");
+		if(username_error_color.equals(loc.getProperty("error_color"))) {
+			System.out.println(username_error_color);
+			softAssert.assertEquals(username_error_color,loc.getProperty("error_color"));
+			softAssert.assertAll();
+		}
+
+
 	}
 
 }
